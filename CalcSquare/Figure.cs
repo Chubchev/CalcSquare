69b2fe5528b0bb3e5d96030921ffdcfd362620af
chub <chub@CHUBCHEV-PC>
2024-11-05 13:22:52 +0300

    Добавьте файлы проекта.

---
+++ CalcSquare/Figure.cs
@@ -0,0 +1,54 @@
+﻿namespace CalcSquare
+{
+    public class Figure
+    {
+        public virtual string Message { get; set; }
+        public virtual double Square { get; set; }
+    }
+    public class Circle : Figure 
+    {
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+        public double Radius { get; set; }
+        public override double Square
+        {
+            get
+            {
+                return Math.PI * Radius * Radius;
+            }
+        }
+    }
+    public class Triangle : Figure
+    {
+        public Triangle(double a, double c, double b)
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
+        public double A { get; set; }
+        public double B { get; set; }
+        public double C { get; set; }
+        public override double Square
+        {
+            get
+            {   //по трем сторонам: S = √p · (p — a)(p — b)(p — c),
+                double p = (A + B + C)/2.0; 
+                return (Math.Sqrt(p * (p - A) * (p - B) * (p - C)));
+            }
+        }
+        public override string Message
+        {
+            get
+            {
+                //прямоугольный ли?
+                return A != 0 && B != 0 && C != 0 && (Math.Pow(A, 2) == Math.Pow(B, 2) + Math.Pow(C, 2) ||
+                                                      Math.Pow(B, 2) == Math.Pow(A, 2) + Math.Pow(C, 2) ||
+                                                      Math.Pow(C, 2) == Math.Pow(B, 2) + Math.Pow(A, 2)) ? 
+                   "Заданный треугольник является прямоугольным" : "";
+            }               
+        }
+    }
+}
---
+++ TestCalcSquare/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CalcSquare;
+
+namespace TestCalcSquare
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        enum FUGURE { CIRCLE, TRIANGLE }
+        private FUGURE FigureType;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnType_Checked(object sender, RoutedEventArgs e)
+        {
+            int figureType = 0;
+            int.TryParse((string)((RadioButton)sender).Tag, out figureType);
+            FigureType = (FUGURE)figureType;
+            if (textBlock_Result1 != null)
+            {
+                textBlock_Result1.Text = "";
+                textBlock_Result2.Text = "";
+            }
+        }
+
+        private void CalcButton_Click(object sender, RoutedEventArgs e)
+        {
+            string FigureName = "";
+            CalcSquare.Figure? figure = null;
+            switch (FigureType)
+            {   
+                case  FUGURE.CIRCLE:
+                    double Radius = 0;
+                    FigureName = "круга: ";
+                    double.TryParse(textBox_Radius.Text, out Radius);
+                    if(Radius==0)
+                    {
+                        textBlock_Result2.Text = "Введите ненулевой радиус";
+                        return;
+                    }
+                    figure = new CalcSquare.Circle(Radius);
+                    break;
+                case FUGURE.TRIANGLE:
+                    double A = 0, B = 0, С = 0;
+                    FigureName = "треугольника: ";
+                    double.TryParse(textBox_A.Text, out A);
+                    double.TryParse(textBox_B.Text, out B);
+                    double.TryParse(textBox_C.Text, out С);
+                    if (A == 0 || B == 0 || С == 0)
+                    {
+                        textBlock_Result2.Text = "Введите ненулевую длину сторон";
+                        return;
+                    }
+                    figure = new CalcSquare.Triangle(A, B, С);
+                    break;
+                default:
+                    textBlock_Result2.Text = "Расчёт полощади заданной фигуры не реализован";
+                    return;
+
+            }
+            if (figure != null)
+            {
+                textBlock_Result1.Text = "Площадь "+ FigureName + figure.Square.ToString();
+                textBlock_Result2.Text = FigureType == FUGURE.TRIANGLE ? figure.Message : "";
+            }
+        }
+    }
+
+    /// <summary>конвертер bool в видимость контрола</summary>
+    [ValueConversion(typeof(bool), typeof(Visibility))]
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        //OneWay
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return ((bool)value) ? Visibility.Visible : Visibility.Collapsed;
+        }
+        //OneWayToSource
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return 0;
+        }
+    }
+}
